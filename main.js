418f1d98a6f892173f86f33fd7f83490be59deb7
adriano-sena <58452965+adriano-sena@users.noreply.github.com>
2021-05-15 21:42:44 -0300

    Data Biding directive

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'Meias quentinhas para o inverno',
+        image: './assets/socks.jpg',
+        altText: 'Beautiful socks',
+        link: 'http://google.com'
+    }
+})
